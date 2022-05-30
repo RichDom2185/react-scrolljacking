efcf34a64868e42a5fec7c76f3f25f850173e5b7
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2022-05-30 08:20:51 +0800

    Add PopInImage

--- src/components/PopInImage.tsx
+++ src/components/PopInImage.tsx
@@ -1,12 +1,50 @@
+import { useEffect, useState } from "react";
+
 type Props = {
+  id: string;
   from: "left" | "right";
   src: string;
 };
 
-const PopInImage: React.FC<Props> = (props) => {
+const PopInImage: React.FC<Props> = ({ id, from, src }) => {
+  const [percentage, setPercentage] = useState(0);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const handleScroll = (e: Event) => {
+    setPercentage(getPercentage());
+  };
+
+  const getPercentage = () => {
+    const img = document.getElementById(id);
+    const boundingBox = img!.getBoundingClientRect();
+    const target = boundingBox.top + (3 * boundingBox.height) / 4;
+    const transitionstart = boundingBox.top + (1 * boundingBox.height) / 4;
+    const transitionHeight = target - transitionstart;
+    return (
+      Math.min(
+        -Math.min(transitionstart - window.innerHeight, 0) / transitionHeight,
+        1
+      ) * 100
+    );
+  };
+
   return (
     <div className="pop__container">
-      <img className="pop__image" src={props.src} alt="Decoration" />
+      <img
+        id={id}
+        className="pop__image"
+        src={src}
+        alt="Decoration"
+        style={{
+          opacity: `${percentage}%`,
+        }}
+      />
     </div>
   );
 };
