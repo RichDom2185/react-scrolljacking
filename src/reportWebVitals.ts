87005c69b19d1549140ce84cebb5e0881072d553
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2025-03-31 17:34:54 +0800

    Upgrade web-vitals to v4

--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -1,14 +1,14 @@
-import { ReportHandler } from "web-vitals";
+import { ReportCallback } from "web-vitals";
 
-const reportWebVitals = (onPerfEntry?: ReportHandler) => {
+const reportWebVitals = (onPerfEntry?: ReportCallback) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     // eslint-disable-next-line @typescript-eslint/no-floating-promises
-    import("web-vitals").then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
+    import("web-vitals").then(({ onCLS, onFID, onFCP, onLCP, onTTFB }) => {
+      onCLS(onPerfEntry);
+      onFID(onPerfEntry);
+      onFCP(onPerfEntry);
+      onLCP(onPerfEntry);
+      onTTFB(onPerfEntry);
     });
   }
 };
